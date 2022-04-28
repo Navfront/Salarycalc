2deb4a98cf2091b6f332c61d420692a194d5e3f2
Navfront <b3rt@yandex.ru>
2022-04-28 21:20:49 +0300

    storybook: provide redux context

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,21 @@
-import React from "react";
-import { addDecorator } from "@storybook/react";
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./../src/theme/defaultTheme";
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './../src/theme/defaultTheme';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './../src/redux/rootReducer';
 
-addDecorator((story) => <ThemeProvider theme={defaultTheme}>{story()}</ThemeProvider>);
+const store = createStore(rootReducer);
+
+addDecorator((story) => (
+  <ThemeProvider theme={defaultTheme}>
+    <Provider store={store}>{story()}</Provider>
+  </ThemeProvider>
+));
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
