adba4304e7381d4bb433ccf1eae85a3df419ff4b
Navfront <b3rt@yandex.ru>
2022-04-30 20:22:20 +0300

    monthfilter done

--- src/stories/layouts/main/Main.jsx
+++ src/stories/layouts/main/Main.jsx
@@ -7,15 +7,30 @@ import MonthFilter from './../../ui/month-filter/MonthFilter';
 
 function Main() {
   const calendar = useSelector((state) => state.calendarReducer);
+  const filter = useSelector((state) => state.appReducer.monthFilter);
+
+  const canIRender = (index) => {
+    switch (filter.showType) {
+      case 0:
+        return true;
+      case 1:
+        return index === filter.showMonth || index === filter.showMonth - 1;
+      case 2:
+        return index === filter.showMonth;
+
+      default:
+        return false;
+    }
+  };
 
   return (
     <StyledMain>
       <Container>
         <MonthFilter />
-        {calendar.map((item, index) => (
+        {calendar.map((item, index) =>
           // eslint-disable-next-line react/no-array-index-key
-          <WorkCalendar title={MONTHS[index]} key={index} month={index} />
-        ))}
+          canIRender(index) ? <WorkCalendar title={MONTHS[index]} key={index} month={index} /> : null
+        )}
       </Container>
     </StyledMain>
   );
